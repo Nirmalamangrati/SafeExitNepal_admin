d01c6bc2a1f7068065c9b8f3cc7ea5e2c08e0e19
Nirmala Mangrati <nirmalamgrt@gmail.com>
2026-07-29 13:40:33 +0545

    add admin login

--- src/App.tsx
+++ src/App.tsx
@@ -159,7 +159,6 @@ export default function App(): React.JSX.Element {
   ]);
   /*    MANUAL ALERT STATES */
   const [manualType, setManualType] = useState<string>("FLOOD");
-
   const [manualCategory, setManualCategory] = useState<string>("HIGH");
 
   const [locationName, setLocationName] = useState<string>("");
